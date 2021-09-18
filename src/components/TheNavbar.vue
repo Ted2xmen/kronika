4b4c1b911eb11e91a289d20a27a0ef7a0e3211da
Ted <tugrulerdemdogru@gmail.com>
2021-09-18 23:03:44 +0300

    build

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,10 +3,10 @@
 <nav id="nav" class="navbar navbar-expand-lg  bg-nav py-4 ">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">KRONIKA</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon "></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse " id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
              <router-link class="nav-link" to="/">Anasayfa</router-link>
